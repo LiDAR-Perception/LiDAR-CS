a33e6fb0a9dcdd619a5786e9fb2d83758bdcf3be
LiDAR-Perception <94584481+LiDAR-Perception@users.noreply.github.com>
2021-12-16 11:03:02 +0800

    Create download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,22 @@
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/Livox.tar.gz000
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/Livox.tar.gz001
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/Livox.tar.gz002
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/Livox.tar.gz003
+
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/ONCE-40.tar.gz000
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/ONCE-40.tar.gz001
+
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-32.tar.gz000
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-32.tar.gz001
+
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64.tar.gz000
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64.tar.gz001
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64.tar.gz002
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64.tar.gz003
+
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz001
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz002
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz003
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz004
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz005
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz006
