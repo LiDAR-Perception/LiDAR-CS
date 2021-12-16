4ee259947be79574079e5b952dc508fa6452c5d0
LiDAR-Perception <94584481+LiDAR-Perception@users.noreply.github.com>
2021-12-16 13:52:58 +0800

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -20,3 +20,24 @@ wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz003
 wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz004
 wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz005
 wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-128.tar.gz006
+
+# different heights
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-1.0m.tar.gz000
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-1.0m.tar.gz001
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-1.0m.tar.gz002
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-1.0m.tar.gz003
+
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-1.5m.tar.gz000
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-1.5m.tar.gz001
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-1.5m.tar.gz002
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-1.5m.tar.gz003
+
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-2.5m.tar.gz000
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-2.5m.tar.gz001
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-2.5m.tar.gz002
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-2.5m.tar.gz003
+
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-3.0m.tar.gz000
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-3.0m.tar.gz001
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-3.0m.tar.gz002
+wget https://ad-apolloscape.cdn.bcebos.com/LiDAR_CS/VLD-64-3.0m.tar.gz003
